4e7834c49663747a259f95b72edd2c2326425064
Neik <25412853+neilgcns@users.noreply.github.com>
2024-01-05 22:28:07 +0000

    Update maid.sh

--- maid.sh
+++ maid.sh
@@ -25,8 +25,7 @@ SELECTION=$(whiptail --title "Safe Network Testnet" --radiolist \
 "3" "Stop Nodes" OFF \
 "4" "Get Test Coins" OFF \
 "5" "Start Vdash" OFF \
-"6" "Update all and Restart" OFF \
-"7" "Josh comnet" OFF 3>&1 1>&2 2>&3)
+"6" "Update all and Restart" OFF OFF 3>&1 1>&2 2>&3)
 
 if [[ $? -eq 255 ]]; then
 exit 0
@@ -43,17 +42,12 @@ fi
 PORTS_TO_CLOSE=$(ls $HOME/.local/share/safe/node | wc -l)
 sudo ufw delete allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$PORTS_TO_CLOSE-1))/tcp comment 'safe nodes'
 ############################## Stop Nodes and delete safe folder
+
+#install safeup
 curl -sSL https://raw.githubusercontent.com/maidsafe/safeup/main/install.sh | bash
 
 pkill -e safenode
 rm -rf $HOME/.local/share/safe
-
-rm -rf $HOME/ip_cache.txt
-rm -rf $HOME/node_registry.conf
-rm -rf $HOME/resources_*.log
-rm $HOME/ntracking/*.log
-rm -rf $HOME/ntracking/logs
-sudo rm /var/www/ntracking/index.html
 
 sleep 2
 ############################## install client node and vdash
@@ -100,13 +94,6 @@ sudo ufw delete allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$PORTS_TO_CLOSE-1))/t
 pkill -e safenode
 rm -rf $HOME/.local/share/safe
 
-rm -rf $HOME/ip_cache.txt
-rm -rf $HOME/node_registry.conf
-rm -rf $HOME/resources_*.log
-rm $HOME/ntracking/*.log
-rm -rf $HOME/ntracking/logs
-sudo rm /var/www/ntracking/index.html
-
 ######################################################################################################################## Get Test Coins
 elif [[ "$SELECTION" == "4" ]]; then
 NUMBER_COINS=$(whiptail --title "Number of Coins" --inputbox "\nEnter number of deposits 100 each" 8 40 $NUMBER_COINS 3>&1 1>&2 2>&3)
@@ -129,48 +116,5 @@ rustup update
 sudo apt update -y && sudo apt upgrade -y
 sudo reboot
 
-############################################################################################################################################# comnet
-elif [[ "$SELECTION" == "7" ]]; then
-NUMBER_NODES=$(whiptail --title "Number of Nodes to start" --inputbox "\nEnter number of nodes" 8 40 $NUMBER_NODES 3>&1 1>&2 2>&3)
-if [[ $? -eq 255 ]]; then
-exit 0
-fi
-
-############################## count nodes directories and close fire wall
-PORTS_TO_CLOSE=$(ls $HOME/.local/share/safe/node | wc -l)
-sudo ufw delete allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$PORTS_TO_CLOSE-1))/tcp comment 'safe nodes'
-############################## Stop Nodes and delete safe folder
-pkill -e safenode
-rm -rf $HOME/.local/share/safe
-rm -rf $HOME/ntracking/*.log
-
-sleep 2
-############################## install client node and vdash and set up comnet  make sure and change download and unzip instructions and peer
-cd
-mkdir comnet
-cd comnet
-wget https://comnet-sn.s3.eu-west-2.amazonaws.com/maidsafe/main/safe-beta-x86_64-unknown-linux-musl.tar.gz
-wget https://comnet-sn.s3.eu-west-2.amazonaws.com/maidsafe/main/safenode-beta-x86_64-unknown-linux-musl.tar.gz
-sleep 1
-tar -xvzf safe-beta-x86_64-unknown-linux-musl.tar.gz
-tar -xvzf safenode-beta-x86_64-unknown-linux-musl.tar.gz
-sleep 1
-rm *.gz
-
-cargo install vdash
-############################## open ports 
-sudo ufw allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$NUMBER_NODES-1))/tcp comment 'safe nodes'
-sleep 2
-############################## start nodes
-for (( c=$NODE_PORT_FIRST; c<=$(($NODE_PORT_FIRST+$NUMBER_NODES-1)); c++ ))
-do 
-   sleep $NODE_START_DELAY && /home/ubuntu/comnet/safenode --peer /ip4/46.101.5.184/tcp/36301/p2p/12D3KooWBVHdavXJqggn396jQQevJvXtjTN1pwb8YqS65wyitTrx --port $c --max_log_files 2 --max_archived_log_files 1 2>&1 > /dev/null & disown
-   echo "starting node on port $c with $NODE_START_DELAY second delay"
-   NODE_START_DELAY=$(($NODE_START_DELAY+30))
-done
-sleep 2
-
-############################# exit to Vdash
-vdash --glob-path "$HOME/.local/share/safe/node/*/logs/safenode.log"
 
 fi
